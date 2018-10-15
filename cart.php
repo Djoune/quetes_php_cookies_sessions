c02ce6d6421b0ee8ea4f2450525d811bb932cb53
Julie Delmas <julie.delmas33@gmail.com>
2018-10-15 22:29:58 +0200

    Making session's quest.

--- cart.php
+++ cart.php
@@ -1,7 +1,56 @@
-<?php require 'inc/head.php'; ?>
+<?php
+session_start();
+
+if (empty($_SESSION['login'])) {
+    header('Location: login.php');
+}
+if (isset($_SESSION['cookie'])) {
+    $cookie = array_count_values($_SESSION['cookie']);
+}
+require 'inc/head.php';
+ ?>
 <section class="cookies container-fluid">
     <div class="row">
-      TODO : Display shopping cart items from $_SESSION here.
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <figure class="thumbnail text-center">
+                <img src="assets/img/product-46.jpg" alt="cookies choclate chips" class="img-responsive">
+                <figcaption class="caption">
+                    <h3>Pecan nuts</h3>
+                    <p>Cooked by Penny !</p>
+                    <p>Panier : <?= isset($cookie[46])? $cookie[46] : 0  ?></p>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <figure class="thumbnail text-center">
+                <img src="assets/img/product-36.jpg" alt="cookies choclate chips" class="img-responsive">
+                <figcaption class="caption">
+                    <h3>Chocolate chips</h3>
+                    <p>Cooked by Bernadette !</p>
+                    <p>Panier : <?= isset($cookie[36])? $cookie[36] : 0  ?></p>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <figure class="thumbnail text-center">
+                <img src="assets/img/product-58.jpg" alt="cookies choclate chips" class="img-responsive">
+                <figcaption class="caption">
+                    <h3>Chocolate cookie</h3>
+                    <p>Cooked by Bernadette !</p>
+                    <p>Panier : <?= isset($cookie[58])? $cookie[58] : 0  ?></p>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <figure class="thumbnail text-center">
+                <img src="assets/img/product-32.jpg" alt="cookies choclate chips" class="img-responsive">
+                <figcaption class="caption">
+                    <h3>M&M's&copy; cookies</h3>
+                    <p>Cooked by Penny !</p>
+                    <p>Panier : <?= isset($cookie[32])? $cookie[32] : 0  ?></p>
+                </figcaption>
+            </figure>
+        </div>
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
